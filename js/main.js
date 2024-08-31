206a22765559ec22fae44243b80b81e7c17517e2
pin <makarodesa.1@gmail.com>
2024-08-31 14:02:49 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -15,5 +15,5 @@ document.querySelector('.popup-close').addEventListener('click', function() {
 
 
 let popup__link = document.querySelector('.popup-block__link');
-const LINK = 'https://t.me/+rHhGIWfswa8yNDYy';
+const LINK = 'https://t.me/+rL8R9nw9GhpiNDUy';
 popup__link.href = LINK;
